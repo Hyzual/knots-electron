557b1cb933a7d42025f8a18004c3a36d9d5dc1b9
Hyzual <hyzual@gmail.com>
2016-04-02 23:08:17 +0200

    Use gulp for live-reload

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp     = require('gulp');
+var electron = require('electron-connect').server.create();
+
+gulp.task('serve', function () {
+
+  // Start browser process
+  electron.start();
+
+  // Restart browser process
+  // gulp.watch('main.js', electron.restart);
+
+  // Reload renderer process
+  gulp.watch([
+  	'main.js',
+  	'app/index.js',
+  	'index.html'
+  ], electron.reload);
+});
