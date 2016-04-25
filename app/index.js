1893948562ed37e80ba1277d3789c654a27fba1c
Hyzual <hyzual@gmail.com>
2016-04-25 23:55:45 +0200

    Enable choosing a directory and parsing all .js files in it
    Also start using promises

--- app/index.js
+++ app/index.js
@@ -1,25 +1,58 @@
 // require('./style.css');
-var knots  = require('knotsjs');
-var remote = require('remote');
-var dialog = remote.require('dialog');
-var layout = require('./app/layout.js');
-var graph  = require('./app/graph.js');
+var remote    = require('remote');
+var dialog    = remote.require('dialog');
+var Promise   = require('bluebird');
+var recursive = require('recursive-readdir');
+var knots     = require('knotsjs');
+var path      = require('path');
+var layout    = require('./app/layout.js');
+var graph     = require('./app/graph.js');
 
 function openFile() {
   dialog.showOpenDialog({
     filters: [
-      { name: 'js', extensions: ['js'] }
+      { name: 'Javascript files', extensions: ['js'] }
     ]
-  }, parseFiles);
+  }, parseFile);
 }
 
-function parseFiles(filenames) {
-  if (! filenames) {
+function openDirectory() {
+  dialog.showOpenDialog({
+    properties: ['openDirectory']
+  }, parseDirectory);
+}
+
+function parseFile(file_paths) {
+  if (! file_paths) {
     return;
   }
-  console.log('filenames', filenames);
+  console.log('file_paths', file_paths);
 
-  knots.parseFiles(filenames, function(err, dependencies) {
+  parse(file_paths);
+  //TODO: Use knots.parse()
+}
+
+function parseDirectory(directory_paths) {
+  if (! directory_paths) {
+    return;
+  }
+  console.log('directory_paths', directory_paths);
+
+  var directory_path = directory_paths[0];
+
+  // TODO: move this code to knotsjs
+  var recursiveReadDir = Promise.promisify(recursive);
+  recursiveReadDir(directory_path, ['*_test.js', ignoreNonJS]).then(function (file_paths) {
+    return parse(file_paths);
+  });
+}
+
+function ignoreNonJS(file_path) {
+  return path.extname(file_path) !== '.js';
+}
+
+function parse(file_paths) {
+  return knots.parseFiles(file_paths).then(function(dependencies) {
     var width  = 960;
     var height = 500;
 
